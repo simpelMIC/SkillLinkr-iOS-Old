08b27d0d75ae54cfd1e9fc785fdaae90aba80e5b
simpelMIC <mic-pro@online.de>
2024-07-13 11:50:54 +0200

    Initial Commit

---
+++ SkillLinkr/SkillLinkrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillLinkrApp.swift
+//  SkillLinkr
+//
+//  Created by Christian on 13.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SkillLinkrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
